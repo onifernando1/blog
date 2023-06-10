6c420283d9c619cecf22839f5740856037c9b910
onifernando1 <onifernando1@gmail.com>
2023-06-10 23:01:13 +0100

    fix .pug errors

--- blog/app.js
+++ blog/app.js
@@ -81,6 +81,7 @@ app.use((req, res, next) => {
 });
 
 // view engine setup
+app.engine("pug", require("pug").__express);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(logger("dev"));
