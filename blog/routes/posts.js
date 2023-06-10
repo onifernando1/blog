a0ea2c5045e7913487d79a0e858298a329f90519
onifernando1 <onifernando1@gmail.com>
2023-06-10 23:04:23 +0100

    create skeleton post routes

--- blog/routes/posts.js
+++ blog/routes/posts.js
@@ -6,4 +6,19 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+// POST
+router.post("/", (req, res) => {
+  return res.send("POST");
+});
+
+//PUT
+router.put("/postId", (req, res) => {
+  return res.send("PUT");
+});
+
+//DELETE
+router.delete("/postId", (req, res) => {
+  return res.send("DELETE");
+});
+
 module.exports = router;
