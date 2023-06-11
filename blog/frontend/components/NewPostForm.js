6839df734565037d9e671c7c0258c14ae9dec546
onifernando1 <onifernando1@gmail.com>
2023-06-11 21:06:39 +0100

    create new post form, components

---
+++ blog/frontend/components/NewPostForm.js
@@ -0,0 +1,15 @@
+function NewPostForm(params) {
+  return (
+    <>
+      <form>
+        <label>Title</label>
+        <input type="text"></input>
+        <label>Information</label>
+        <input type="text"></input>
+        <label>Author</label>
+        <input type="text"></input>
+        <input type="submit"></input>
+      </form>
+    </>
+  );
+}
---
+++ blog/frontend/components/Post.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState } from "react";
+
+function Post(params) {
+  return <div></div>;
+}
+
+export default Post;
