94f2c3026126cab3f04a3f244b0c52abd2a929fc
onifernando1 <onifernando1@gmail.com>
2023-06-11 21:55:45 +0100

    get form data on submit - new post

--- blog/frontend/src/components/NewPostForm.js
+++ blog/frontend/src/components/NewPostForm.js
@@ -1,7 +1,22 @@
+import { useState, setState } from "react";
+
 function NewPostForm(params) {
+  const [newPostData, setNewPostData] = useState(null);
+
+  const createPost = (e) => {
+    e.preventDefault();
+    const newPost = {
+      title: e.target[0].value,
+      information: e.target[1].value,
+      author: e.target[2].value,
+    };
+    setNewPostData(newPost);
+    alert(newPost);
+    console.log(newPost);
+  };
   return (
     <>
-      <form>
+      <form onSubmit={createPost}>
         <label>Title</label>
         <input type="text"></input>
         <label>Information</label>
