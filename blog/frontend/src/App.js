8285ce3daadd7d025b167de4dccdf39254b72b55
onifernando1 <onifernando1@gmail.com>
2023-06-11 17:07:27 +0100

    fetch api

--- blog/frontend/src/App.js
+++ blog/frontend/src/App.js
@@ -1,5 +1,16 @@
+import React, { useState, useEffect } from "react";
+
 function App() {
-  return <div>Hello</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/posts").then((res) =>
+      res.json().then((data) => setData(data))
+    );
+    console.log(data);
+  }, []);
+
+  return <div>HEYA</div>;
 }
 
 export default App;
