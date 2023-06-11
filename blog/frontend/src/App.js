2dbd21199744165b1782db7931797e6aa6efdc7e
onifernando1 <onifernando1@gmail.com>
2023-06-11 17:15:14 +0100

    fix fetch and display post ids on react front end !

--- blog/frontend/src/App.js
+++ blog/frontend/src/App.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [postList, setPostList] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:9000/posts").then((res) =>
-      res.json().then((data) => setData(data))
+      res.json().then((data) => setPostList(data.post_list))
     );
-    console.log(data);
   }, []);
 
-  return <div>HEYA</div>;
+  return (
+    <div>
+      HEYA
+      {postList && (
+        <ul>
+          {postList.map((post) => (
+            <li key={post._id}>{post._id}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
