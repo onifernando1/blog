325a6080287d9dda9f2ffccf51f114d01657c695
onifernando1 <onifernando1@gmail.com>
2023-06-12 15:42:29 +0100

    handleChange on login form

--- blog/frontend/src/components/LogInForm.js
+++ blog/frontend/src/components/LogInForm.js
@@ -2,13 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 function LogInForm(params) {
+  const [loginName, setLoginName] = useState("");
+  const [loginUserame, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
   return (
     <>
       <form>
         <label name="username">Username</label>
-        <input type="text" name="username"></input>
+        <input
+          type="text"
+          name="username"
+          onChange={(e) => setLoginName(e.target.value)}
+        ></input>
         <label name="password">Password</label>
-        <input type="password" name="password"></input>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setLoginPassword(e.target.value)}
+        ></input>
         <input type="submit"></input>
       </form>
     </>
