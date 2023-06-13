29b2e748f9ca2bae7ca10464ca03562552d061fe
onifernando1 <onifernando1@gmail.com>
2023-06-13 16:28:24 +0100

    home.css created

--- blog/frontend/src/pages/Home.js
+++ blog/frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useState, setState } from "react";
 import axios from "axios";
+import "../assets/styles/home.css";
 
 function Home(params) {
   axios.defaults.withCredentials = true;
