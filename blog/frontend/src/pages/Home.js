1a1cf6de15d8e94b545e8fdb619e786139e3a4df
onifernando1 <onifernando1@gmail.com>
2023-06-13 15:55:58 +0100

    start creating home page, place into app

---
+++ blog/frontend/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+
+function Home(params) {
+  return (
+    <div>
+      <div>Oni Fernando blogs</div>
+      <div className="blog-home-container">
+        <div>Individual blog</div>
+        <div>Individual blog</div>
+        <div>Individual blog</div>
+        <div>Individual blog</div>
+        <div>Individual blog</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
