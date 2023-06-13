12495a3234027415f8c63de5ee0ee4f5404190dc
onifernando1 <onifernando1@gmail.com>
2023-06-13 16:25:49 +0100

    show image, map through array of posts

--- blog/frontend/src/pages/Home.js
+++ blog/frontend/src/pages/Home.js
@@ -1,17 +1,42 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect } from "react";
+import { useState, setState } from "react";
+import axios from "axios";
 
 function Home(params) {
+  axios.defaults.withCredentials = true;
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/posts")
+      .then((response) => {
+        console.log("Login response:", response);
+        setPosts(response.data.post_list);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
       <div className="home-container">
         <div>Oni Fernando blogs</div>
         <div className="blog-home-container">
-          <div>Individual blog</div>
-          <div>Individual blog</div>
-          <div>Individual blog</div>
-          <div>Individual blog</div>
-          <div>Individual blog</div>
+          {posts.map((post) => {
+            return (
+              <>
+                <div>
+                  <img
+                    src={require(`../assets/images/${post.image}.jpg`)}
+                  ></img>
+                </div>
+                <div>{post.title}</div>
+                <div>{post.information}</div>
+              </>
+            );
+          })}
         </div>
       </div>
     </div>
