cf40edcf4d1f25feebcd0088959338755c98b917
onifernando1 <onifernando1@gmail.com>
2023-06-17 23:31:19 +0100

    copy styles to sign up form

--- blog/frontend/src/components/SignUpForm.js
+++ blog/frontend/src/components/SignUpForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, setState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function SignUpForm(params) {
   const [registerName, setRegisterName] = useState("");
@@ -36,7 +37,59 @@ function SignUpForm(params) {
 
   return (
     <>
-      <form onSubmit={register}>
+      <div className="login-page-container">
+        <div className="login-image">
+          <img src={require(`../assets/images/login.jpg`)}></img>
+        </div>
+        <div className="login-form-container">
+          <div className="form-header"></div>
+
+          <form onSubmit={register}>
+            <div className="sign-in-link">SIGN UP</div>
+            <div className="username-container">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={(e) => setRegisterName(e.target.value)}
+              ></input>
+            </div>
+            <div className="username-container">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                onChange={(e) => setRegisterUsername(e.target.value)}
+              ></input>
+            </div>
+            <div className="password-container">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={(e) => setRegisterPassword(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <input
+                className="login-submit"
+                type="submit"
+                value="SIGN UP"
+              ></input>
+            </div>
+            <Link to="/login">
+              <div className="sign-up-link-container">
+                Already have an account?
+              </div>
+            </Link>
+          </form>
+        </div>
+      </div>
+
+      {/* <form onSubmit={register}>
         <label htmlFor="name">Name</label>
         <input
           type="text"
@@ -59,10 +112,7 @@ function SignUpForm(params) {
           onChange={(e) => setRegisterPassword(e.target.value)}
         ></input>
         <input type="submit"></input>
-      </form>
-      {/* <div>{registerName}</div> */}
-      <div>{currentUser}</div>
-      <div>aa</div>
+      </form> */}
     </>
   );
 }
